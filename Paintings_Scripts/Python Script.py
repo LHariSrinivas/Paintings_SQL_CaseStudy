b0e29b9af862b52ee3b5f7cdda7533a9852a32c5
LHariSrinivas <87022053+LHariSrinivas@users.noreply.github.com>
2024-01-30 23:23:48 +0530

    Add files via upload
    This is the Python Script to convert CSV data into SQL Data. This step is crucial before starting the project since we are querying in MySQL and the data should be in a readable format for MySQL since MySQL cannot read CSV data directly.

---
+++ Paintings_Scripts/Python Script.py
@@ -0,0 +1,17 @@
+#Connecting Python to MySQL Database
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+conn_string = "mysql+mysqlconnector://root:hari1672*@localhost:3306/paintings"
+            #mysql+<drivername>://<username>:<password>@<server>:<port>/dbname
+db = create_engine(conn_string)
+conn = db.connect()
+
+#Load CSV files (we will looping through all the files. alterantive option is that we can write the same piece of code n times for n files in the dataset)
+
+files = ['artist','canvas_size','image_link','museum','museum_hours','product_size','subject','work']
+for file in files:
+    df = pd.read_csv(f"C:/Users/SHREE/Desktop/Portfolio Projects/SQL Projects or Case Studies/Project 1/Datasets/{file}.csv")
+    #Convert the Pandas DataFrame to a Format for MySQL Table Insertion
+    df.to_sql(file,con = conn, if_exists = 'replace', index=False)
